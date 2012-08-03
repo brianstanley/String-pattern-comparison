b02f1fdb5b5bba25440597d67c9058dcadf0dfc1
Brian <brianelblog@gmail.com>
2012-08-03 13:27:10 -0300

    First files upload

---
+++ script.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Script that compare strings with a pattern and return
+the similarity results
+For the comparisson You can put an array or a single string for the compare word
+The pattern its a simple string but with some modifications you can put an array.
+--------------------
+IMPORTANT READ
+100%: The words are same(best).
+>100%: The words are little similar but not same.
+
+*/
+class Analysis{
+	public $patron = "comunidad";
+	public $busqueda;
+	public $busquedaLen;
+	public $patronLen;
+/* start methods */	
+	function __construct($busqueda,$pattern){
+		/*Assing values to propierties */
+		$this->patron=$pattern;
+		/*Here i delete blank spaces if passed an array with comparison values */
+		if(is_array($busqueda)):
+			array_walk($busqueda, array($this,'trimPalabras'));
+		endif;
+		$this->busqueda=$busqueda;
+			
+	}
+	function trimPalabras(&$value){ 
+	    $value = trim($value); 
+	}
+
+	function starter(){
+		if(is_array($this->busqueda)):
+			foreach($this->busqueda as $palabra){
+				$this->busqueda = $palabra;
+				if(strlen($palabra) > strlen($this->patron)):
+					$resto = strlen($palabra)-strlen($this->patron); 	
+					$palabra=substr_replace($palabra, "", -$resto);
+				endif;
+				$this->busquedaLen = strlen($palabra);
+				$this->patronLen = strlen($this->patron);		
+				$diferencia = $this->patronLen-$this->busquedaLen;
+				if($diferencia > 0){
+					for($i=1;$i<=$diferencia;$i++){
+						$palabra = $palabra." ";	
+					}
+				}
+				$this->coincidences($palabra,$this->patron);			
+				
+			}
+		else:
+			$busqueda = $this->busqueda;
+			$this->busquedaLen = strlen($this->busqueda);
+			$this->patronLen = strlen($this->patron);
+			$diferencia = $this->patronLen-$this->busquedaLen;
+			if($diferencia > 0){
+				for($i=1;$i<=$diferencia;$i++){
+					$busqueda = $this->busqueda." ";	
+				}
+			}
+			$this->coincidences($busqueda,$this->patron);	
+		endif;
+	}
+	/* Here I get the individually percentage of each pattern letter */
+	function getLetterValue(){
+		return 100/$this->patronLen;
+	}
+	/*Here I obtain the value of the comparison word with the pattern */
+	function comparison(){
+		$valeCletra = 100/$this->patronLen;
+		$res = $valeCletra*$this->busquedaLen;
+		return $res;
+	}
+	function coincidences($palabra){
+		$coincidences = 0;
+		$palabra = " ".$palabra; 
+		for($in = 1;$in<=strlen($this->patron);$in++){
+			for($a=1; $a<=strlen($this->patron);$a++){
+				if($palabra[$a]==$this->patron[$in-1] ):
+					$palabra[$a]="|";	
+					$coincidences++;
+				endif;	
+			}	
+		}
+		$valorLetra = $this->getLetterValue($this->patronLen); 
+		echo "Palabra: <b>".$this->busqueda."</b> | Patron: <b>".$this->patron."</b><br>";
+		echo "<br>";
+
+		echo "Resultado final: ";
+		$rfinal = $coincidences*$valorLetra+$this->comparison($this->busquedaLen,$this->patronLen);
+		$rfinal = $rfinal/2;
+		echo $rfinal."% de posibilidades";
+		echo "<br>";
+		echo "<br>";
+	}
+/* End Methods */
+
+}
+/* End class analysis */
+$busqueda=array("bebo?","como?","como","comunidades");
+//$busqueda = "comunity";
+$pattern = "comunity";
+$analisis = new Analysis($busqueda,$pattern);
+$analisis->starter();
+?>
